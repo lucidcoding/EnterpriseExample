217cdad9f58e490844d193426444c97e093a8a4b
Paul Davies <pd_account_2@hotmail.com>
2013-01-03 08:45:48 +0000

    Changed saga to use setter injection.

--- ClientServices.MessageHandlers/Core/MessageHandlersRegistry.cs
+++ ClientServices.MessageHandlers/Core/MessageHandlersRegistry.cs
@@ -1,16 +1,19 @@
 ﻿using ClientServices.Data.Core;
+using ClientServices.Domain.RepositoryContracts;
 using StructureMap.Configuration.DSL;
 
 namespace ClientServices.MessageHandlers.Core
 {
-    class MessageHandlersRegistry : Registry
+    public class MessageHandlersRegistry : Registry
     {
         public MessageHandlersRegistry()
         {
             Configure(x =>
-            {
-                x.ImportRegistry(typeof(DataRegistry));
-            });
+                          {
+                              x.ImportRegistry(typeof (DataRegistry));
+                              SetAllProperties(y => y.OfType<IClientRepository>());
+                              SetAllProperties(y => y.OfType<IServiceRepository>());
+                          });
         }
     }
 }
